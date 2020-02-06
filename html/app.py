59d16c9ea23cdf92981acd07e722d5de7edd025d
dzuml97 <mus.limova_liya@mail.ru>
2020-02-06 19:17:47 +0300

    06.02.20

--- html/app.py
+++ html/app.py
@@ -1,43 +1,58 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from sqlalchemy.exc import IntergrityError
-from models import add_user
+
+from models import add_user, check_user
 
 app = Flask(__name__)
 app.secret_key = 'olujnybhgtvolimujhbtgv'
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "POST" :
+    if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
-        password_check = request.form['password.check']
-        if password_check != password:
-            return render_template('index.html', error="password_not_match")
+        repassword = request.form['repassword']
+        if password != repassword:
+            return render_template('index.html', error='repass_error')
         try:
             add_user(name, email, password)
-        except IntergrityError:
-            return render_template('index.html', error="already_exists")
-        session['username'] = name    
+        except IntegrityError:
+            return render_template('index.html', error='db_error')
+        session['username'] = name
+
         return redirect(url_for('user_page', name=name))
     return render_template('index.html')
 
-@app.route('/users/<name>')    
-def user_page(name):
-    return render_template('user.html', username=name)
 
-@app.route('\login', methods=['GET','POST'])
+@app.route('/users/<name>', method=['GET', 'POST'])
+def user_page(name):
+    if request.method == 'POST':
+        title = request.form['title']
+        details = request.form['details']
+        deadline_date = request.form['deadline_date']
+        add_task(title, details, deadline_date)
+    return render_template('users.html', username=name)
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         user = check_user(email, password)
         if user:
-            session['username'] = name
+            session['username'] = user.name
             return redirect(url_for('user_page', name=user.name))
         else:
-            return render_template('login.html', error=True)
-    
+            render_template('login.html', error=True)
     return render_template('login.html')
 
-app.run(debug=True)    
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+app.run(debug=True)
